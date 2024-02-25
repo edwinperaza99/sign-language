78d27a6316a63297da436a5408f46a813998a8a3
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-02-24 20:21:39 -0800

    updated navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -6,18 +6,18 @@ const Navbar: React.FC = () => {
 	return (
 		<nav className="flex items-center place-content-between">
 			<h1 className="text-3xl">App Logo</h1>
-			<ul className="flex flex-row gap-6 items-center border-2 border-green-400 p-4">
-				<li>
+			<ul className="flex flex-row gap-6 items-center">
+				<li className="w-40 h-20 flex justify-center items-center text-sky-200 border-2 rounded-lg border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
 					<Link href="/">Home</Link>
 				</li>
-				<li>
+				<li className="w-40 h-20 flex justify-center items-center text-sky-200 border-2 rounded-lg border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
 					<Link href="/quiz">Quiz</Link>
 				</li>
-				<li>
-					<Link href="/">Practice ASL with AI</Link>
+				<li className="w-40 h-20 flex justify-center items-center text-sky-200 border-2 rounded-lg border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
+					<Link href="/practice">Practice ASL with AI</Link>
 				</li>
-				<li>
-					<Link href="/">Video Call</Link>
+				<li className="w-40 h-20 flex justify-center items-center text-sky-200 border-2 rounded-lg border-sky-200 shadow-[0_0_2px_#fff,inset_0_0_2px_#fff,0_0_5px_#08f,0_0_15px_#08f,0_0_30px_#08f]">
+					<Link href="/video-call">Video Call</Link>
 				</li>
 				<li>
 					<Link href="/">
